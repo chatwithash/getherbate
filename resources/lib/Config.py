54c6a1ab3c7d4014b40add18e37e628e7fd5bb16
chatwithash <user@localhost>
2020-03-14 20:41:01 +0000

    initial

---
+++ resources/lib/Config.py
@@ -0,0 +1,7 @@
+# -*- coding=utf8 -*-
+#******************************************************************************
+# Config.py
+#------------------------------------------------------------------------------
+
+CHATURBATE_API = "https://chaturbate.com/api/chatvideocontext"
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
